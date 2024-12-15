312ecbb20d9234a47249f568ea6cc1e4efa7e29f
MrNaif2018 <chuff184@gmail.com>
2024-12-16 01:41:19 +0300

    Fix deprecation warnings

--- universalasync/utils.py
+++ universalasync/utils.py
@@ -1,18 +1,6 @@
 import asyncio
 
 
-def _get_event_loop() -> asyncio.AbstractEventLoop:
-    try:
-        current_loop = asyncio._get_running_loop()
-        if current_loop is not None:
-            return current_loop
-        return asyncio.get_event_loop_policy().get_event_loop()
-    except RuntimeError:
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-        return loop
-
-
 def get_event_loop() -> asyncio.AbstractEventLoop:
     """Useful utility for getting event loop. Acts like get_event_loop(), but also creates new event loop if needed
 
@@ -21,8 +9,9 @@ def get_event_loop() -> asyncio.AbstractEventLoop:
     Returns:
         asyncio.AbstractEventLoop: event loop
     """
-    loop = _get_event_loop()
-    if loop.is_closed():
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
+    current_loop = asyncio._get_running_loop()
+    if current_loop is not None:
+        return current_loop
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
     return loop
