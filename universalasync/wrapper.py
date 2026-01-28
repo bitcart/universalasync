3d6b5fe753e2404a3559937ac7fa5b11c6ea433a
Daniel Bernstein <daniel.bernstein@mobileye.com>
2026-01-28 10:50:30 +0200

    Fix ruff format

--- universalasync/wrapper.py
+++ universalasync/wrapper.py
@@ -3,7 +3,7 @@ import functools
 import inspect
 import types
 from collections.abc import AsyncGenerator, Callable, Generator
-from typing import Any, cast, TypeVar
+from typing import Any, TypeVar, cast
 
 from universalasync.utils import get_event_loop
 
